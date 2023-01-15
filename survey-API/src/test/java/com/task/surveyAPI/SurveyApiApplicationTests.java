fff835140ebff06edc104919406fb3179423eafe
Saad Alsubaie <85945978+saad-almlehe@users.noreply.github.com>
2023-01-15 21:11:17 +0300

    starter commit

---
+++ survey-API/src/test/java/com/task/surveyAPI/SurveyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.surveyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
